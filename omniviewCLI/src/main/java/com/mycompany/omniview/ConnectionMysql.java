c51315c9a586b3965f59d82aa87d6b87eefc2dfd
PedroTresmondi <pedro.tresmondi@bandtec.com.br>
2022-05-22 23:58:34 -0300

    Adicionando connection com o mysql :wrench:

--- omniviewCLI/src/main/java/com/mycompany/omniview/ConnectionMysql.java
+++ omniviewCLI/src/main/java/com/mycompany/omniview/ConnectionMysql.java
@@ -4,9 +4,36 @@
  */
 package com.mycompany.omniview;
 
-/**
- *
- * @author pedro
- */
+import org.apache.commons.dbcp2.BasicDataSource;
+
 public class ConnectionMysql {
+
+    private BasicDataSource dataSource;
+
+    public ConnectionMysql() {
+        try {
+            this.dataSource = new BasicDataSource();
+            this.dataSource.setDriverClassName("org.h2.Driver");
+            this.dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+            //this.datasource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            //this.dataSource.setUrl("jdbarh2:file:./meu_banco");
+            this.dataSource.setUrl("jdbc:mysql:// localhost:3306/omniview?useTimezone-true&serverTimezone-UTC");
+         
+            // usuário padrao2
+            this.dataSource.setUsername("sa");
+            // senha padrao2
+            this.dataSource.setPassword("");
+            // Não irá funcionar se você não criar um container exatamente igual o meu.
+            // usuário utilizado em um container MySQL
+            this.dataSource.setUsername("root");
+            // senha padrao utilizada no container MySQL
+            this.dataSource.setPassword("root");
+            // Azure
+            //this.datasource.setUsername("admin-Monitech");
+            //this.datasource.setPassword("2ads#grupo3");
+        } catch (Exception e) {
+            System.out.println("erro ao conectar com o mysql");
+
+        }
+    }
 }
